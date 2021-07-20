602dd304f9242dfb5d072233976af8d7bb89729e
Rodrigo Nunes Leite <rodrigonl10.rn@gmail.com>
2021-07-19 22:12:18 -0300

    -CRIANDODESCONTOSEINGRESSO

---
+++ src/main/java/br/com/caelum/ingresso/model/Ingresso.java
@@ -0,0 +1,30 @@
+package br.com.caelum.ingresso.model;
+
+import java.math.BigDecimal;
+
+import br.com.caelum.ingresso.model.descontos.Desconto;
+
+public class Ingresso {
+	private Sessao sessao;
+	private BigDecimal preco = BigDecimal.ZERO;
+	
+	public Ingresso(Sessao sessao, Desconto tipoDeDesconto) {
+		this.setSessao(sessao);
+		this.preco = tipoDeDesconto.aplicarDescontoSobre(sessao.getPreco());
+	}
+	public BigDecimal getPreco() {
+		return preco;
+	}
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+	
+	public Sessao getSessao() {
+		return sessao;
+	}
+	public void setSessao(Sessao sessao) {
+		this.sessao = sessao;
+	}
+	
+	
+}
